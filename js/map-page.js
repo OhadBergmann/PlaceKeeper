2cad4021cad929a3ec95cfdc4fd7c1cc5f4c3bf2
ohad bergmann <ohad.bergmann#gmail.com>
2022-10-19 02:20:36 +0300

    alpha 0.1

--- js/map-page.js
+++ js/map-page.js
@@ -1,6 +1,16 @@
 'use strict'
 
+var gMap;
 
 function onInitMap(){
-    
+    gMap = generateNewMap();
 }
+
+function initMap() {
+    return map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 8,
+      });
+}
+
+window.initMap = initMap;
